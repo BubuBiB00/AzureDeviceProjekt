90f4fd20caad610f694afd7e9e23f645d4af3457
BuildTools <unconfigured@null.spigotmc.org>
2022-03-31 10:26:44 +0200

    added Coffeemachine

---
+++ Coffeemachine.py
@@ -0,0 +1,16 @@
+from azure.iot.device import IoTHubDeviceClient 
+from random import random 
+
+
+connection_string = "HostName=iot3bhwii22-dz.azure-devices.net;DeviceId=Notebook-dz;SharedAccessKey=cGXDrnQ85YU9fNj1zog8r7Q7Da6avBAlqpsz0gnaFBs=" 
+client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+
+
+def main(): 
+
+    try: 
+        pass
+    except: 
+        pass
+        
+if __name__ == '__main__': main()
