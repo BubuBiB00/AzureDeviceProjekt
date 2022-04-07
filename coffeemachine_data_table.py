ff7c187b3be14296cc91eab38539007be193c5b1
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-04-07 11:17:15 +0200

    coffeemachine_data_table.py

--- coffeemachine_data_table.py
+++ coffeemachine_data_table.py
@@ -3,8 +3,8 @@ from azure.data.tables import TableEntity
 from azure.core.credentials import AzureNamedKeyCredential
 
 
-credential = AzureNamedKeyCredential("coffeemachinedata", "mykey")
-table_service = TableServiceClient(endpoint="mytableendpoint", credential=credential)
+credential = AzureNamedKeyCredential("coffeemachinedata", "mykey") #mykey
+table_service = TableServiceClient(endpoint="mytableendpoint", credential=credential) #mytableendpoint
 table_service = TableServiceClient.from_connection_string(conn_str='DefaultEndpointsProtocol=https;AccountName=myaccount;AccountKey=mykey;TableEndpoint=mytableendpoint;')
 table_service.create_table('coffemachineTable')
 
