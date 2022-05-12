772e5fe4126c7d839f8c31cc7a47fb6bd7c6754b
= <=>
2022-05-12 11:05:36 +0200

    data

--- Coffeemachine.py
+++ Coffeemachine.py
@@ -1,7 +1,7 @@
 from azure.iot.device import IoTHubDeviceClient
 from azure.iot.hub import IoTHubRegistryManager
 from random import randrange
-
+import json
 
 CONNECTION_STRING = "HostName=iot3bhwii22-dz.azure-devices.net;DeviceId=Notebook-dz;SharedAccessKey=cGXDrnQ85YU9fNj1zog8r7Q7Da6avBAlqpsz0gnaFBs=" 
 DEVICE_ID = "Notebook-dz"
@@ -14,22 +14,27 @@ def coffeemachines():
     coffeemachine_coffee = ["Schwarz","Capuccino","Grosser Brauner","Kleiner Brauner","Verlaengerter"]
     coffeemachine_coffee_num = 0
 
-    coffeemachine_msg = [coffeemachine_id[randrange(0,len(coffeemachine_id))],coffeemachine_coffee[randrange(0,len(coffeemachine_coffee))],coffeemachine_coffee_num]
-    print(coffeemachine_msg)
-    return coffeemachine_msg
+    coffeemachine_msg = ""+str(coffeemachine_id[randrange(0,len(coffeemachine_id))])+str(coffeemachine_coffee[randrange(0,len(coffeemachine_coffee))])+str(coffeemachine_coffee_num)
+
+
+    coffemachine_json = json.dumps(coffeemachine_msg)
+
+    print(coffemachine_json)
+    return coffemachine_json
 
 
 def main(): 
     global coffeemachine_coffee_num
 
-    try: 
-        registry_manager = IoTHubRegistryManager("HostName=iot3bhwii22-dz.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=2lJeWgm6IkzOHFlaeM3/4pQH+sX3SMy1sOM9Ao5XMd0=")
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    client.connect()
+    
+    coffeemachine_data = coffeemachines()
 
-        data = coffeemachines()
-        registry_manager.send_c2d_message(DEVICE_ID, data)
-        print("Message sent!")
-    except KeyboardInterrupt: 
-        print("Stopped!")
+    client.send_message(coffeemachine_data)
+    client.disconnect()
+    client.shutdown()
+    print("successfully sent")
         
 if __name__ == '__main__':
     main()
