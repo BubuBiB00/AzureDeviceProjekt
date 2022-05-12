6a119649416cf89fd2e4057b2bdd334f774d573d
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-05-12 10:14:51 +0200

    func

--- ServiceBusTrigger/__init__.py
+++ ServiceBusTrigger/__init__.py
@@ -1,8 +1,31 @@
+#import logging
+
+#import azure.functions as func
+
+
+#def main(msg: func.ServiceBusMessage):
+#    logging.info('Python ServiceBus queue trigger processed message: %s',
+#                 msg.get_body().decode('utf-8'))
+
 import logging
+import json
 
 import azure.functions as func
 
+def main(msg: func.QueueMessage):
+    logging.info('Python queue trigger function processed a queue item.')
 
-def main(msg: func.ServiceBusMessage):
-    logging.info('Python ServiceBus queue trigger processed message: %s',
-                 msg.get_body().decode('utf-8'))
+    result = json.dumps({
+        'id': msg.id,
+        'body': msg.get_body().decode('utf-8'),
+        'expiration_time': (msg.expiration_time.isoformat()
+                            if msg.expiration_time else None),
+        'insertion_time': (msg.insertion_time.isoformat()
+                           if msg.insertion_time else None),
+        'time_next_visible': (msg.time_next_visible.isoformat()
+                              if msg.time_next_visible else None),
+        'pop_receipt': msg.pop_receipt,
+        'dequeue_count': msg.dequeue_count
+    })
+
+    logging.info(result)
