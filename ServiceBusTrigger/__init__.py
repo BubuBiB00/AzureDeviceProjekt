685c53f26dc4d82930f4d9be8ba044fa172efd9a
= <=>
2022-06-02 10:47:15 +0200

    message debug

--- ServiceBusTrigger/__init__.py
+++ ServiceBusTrigger/__init__.py
@@ -1,33 +1,49 @@
 import logging
 import json
 import uuid
-
+#from azure.data.tables import TableServiceClient
+from datetime import datetime
 import azure.functions as func
 
-def main(msg: func.QueueMessage,req: func.QueueMessage, message: func.Out[str]):
+def main(msg: func.ServiceBusMessage):
     logging.info('Python queue trigger function processed a queue item.')
-
-    result = json.dumps({
-        'id': msg.id,
-        'body': msg.get_body().decode('utf-8'),
-        'expiration_time': (msg.expiration_time.isoformat()
-                            if msg.expiration_time else None),
-        'insertion_time': (msg.insertion_time.isoformat()
-                           if msg.insertion_time else None),
-        'time_next_visible': (msg.time_next_visible.isoformat()
-                              if msg.time_next_visible else None),
-        'pop_receipt': msg.pop_receipt,
-        'dequeue_count': msg.dequeue_count
-    })
-
-    logging.info(result)
-
-    rowKey = str(uuid.uuid4())
-
-    data = {
-        "Name": "Output binding message",
-        "PartitionKey": "message",
-        "RowKey": rowKey
-    }
+    message = msg.get_body().decode('utf-8')
     
-    message.set(json.dumps(data))
+    logging.info(message)
+
+    #result = json.dumps({
+    #    u'PartitionKey': Kaffemachine,
+    #    u'RowKey': PRODUCT_ID,
+    #    'message_id': msg.message_id,
+    #    'body': msg.get_body().decode('utf-8'),
+    #    'content_type': msg.content_type,
+    #    'expiration_time': msg.expiration_time,
+    #    'label': msg.label,
+    #    'partition_key': msg.partition_key,
+    #    'reply_to': msg.reply_to,
+    #    'reply_to_session_id': msg.reply_to_session_id,
+    #    'scheduled_enqueue_time': msg.scheduled_enqueue_time,
+    #    'session_id': msg.session_id,
+    #    'time_to_live': msg.time_to_live,
+    #    'to': msg.to,
+    #    'user_properties': msg.user_properties,
+    #    'metadata' : msg.metadata
+    #}, default=str)
+
+    # my_entity = {
+    #     u'PartitionKey': 1234,
+    #     u'RowKey': 1,
+    #     u'Coffeemachine': 15,
+    #     u'Price': 9.99,
+    #     u'Comments': u"great product",
+    #     u'OnSale': True,
+    #     u'ReducedPrice': 7.99,
+    #     u'PurchaseDate': datetime(1973, 10, 4),
+    #     u'BinaryRepresentation': b'product_name'
+    #     }
+
+
+    # table_service_client = TableServiceClient.from_connection_string(conn_str="<connection_string>")
+    # table_client = table_service_client.get_table_client(table_name="myTable")
+
+    # entity = table_client.create_entity(entity=my_entity)
