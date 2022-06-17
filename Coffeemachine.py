78a544f4c00284646e29784b70bc225942d6346e
BubuBiB00 <florenitn.aldrian@ms.bulme.at>
2022-06-17 19:01:16 +0200

    ne function

--- Coffeemachine.py
+++ Coffeemachine.py
@@ -2,19 +2,18 @@ from azure.iot.device import IoTHubDeviceClient
 from random import randrange
 import json
 
-CONNECTION_STRING = "HostName=iot3bhwii22-dz.azure-devices.net;DeviceId=Notebook-dz;SharedAccessKey=cGXDrnQ85YU9fNj1zog8r7Q7Da6avBAlqpsz0gnaFBs=" 
+CONNECTION_STRING = "HostName=iot3bhwii22-dz.azure-devices.net;DeviceId=Notebook-dz;SharedAccessKey=cGXDrnQ85YU9fNj1zog8r7Q7Da6avBAlqpsz0gnaFBs="
 DEVICE_ID = "Notebook-dz"
 client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
+
 def coffeemachines():
-    global coffeemachine_coffee_num
+    coffeemachine_id = [0, 1, 2, 3, 4]
+    coffeemachine_coffee = ["Schwarz", "Capuccino",
+                            "Grosser Brauner", "Kleiner Brauner", "Verlaengerter"]
 
-    coffeemachine_id = [0,1,2,3,4]
-    coffeemachine_coffee = ["Schwarz","Capuccino","Grosser Brauner","Kleiner Brauner","Verlaengerter"]
-    coffeemachine_coffee_num = 0
-
-    coffeemachine_msg = ""+str(coffeemachine_id[randrange(0,len(coffeemachine_id))])+str(coffeemachine_coffee[randrange(0,len(coffeemachine_coffee))])+str(coffeemachine_coffee_num)
-
+    coffeemachine_msg = ""+str(coffeemachine_id[randrange(0, len(coffeemachine_id))])+","+str(
+        coffeemachine_coffee[randrange(0, len(coffeemachine_coffee))])
 
     coffemachine_json = json.dumps(coffeemachine_msg)
 
@@ -22,18 +21,18 @@ def coffeemachines():
     return coffemachine_json
 
 
-def main(): 
-    global coffeemachine_coffee_num
-
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+def main():
+    client = IoTHubDeviceClient.create_from_connection_string(
+        CONNECTION_STRING)
     client.connect()
-    
+
     coffeemachine_data = coffeemachines()
 
     client.send_message(coffeemachine_data)
     client.disconnect()
     client.shutdown()
     print("successfully sent")
-        
+
+
 if __name__ == '__main__':
     main()
--- kaffeebestellttrigger/__init__.py
+++ kaffeebestellttrigger/__init__.py
@@ -1,8 +1,33 @@
 import logging
-
+import json
 import azure.functions as func
+from azure.data.tables import TableServiceClient
+from datetime import datetime
 
 
 def main(msg: func.ServiceBusMessage):
     logging.info('Python ServiceBus queue trigger processed message: %s',
                  msg.get_body().decode('utf-8'))
+
+    connection_string = "HostName=iot3bhwii22-dz.azure-devices.net;DeviceId=Notebook-dz;SharedAccessKey=cGXDrnQ85YU9fNj1zog8r7Q7Da6avBAlqpsz0gnaFBs="
+    table_service_client = TableServiceClient.from_connection_string(
+        conn_str="<connection_string>")
+    table_name = "CoffeemachineDataTable"
+    table_client = table_service_client.create_table(table_name=table_name)
+
+    my_data_json = msg.get_body().decode('utf-8')
+    my_data_string = json.loads(my_data_json)
+
+    Coffeemachine_ID = my_data_string.split(",")[0]
+    Coffee_name = my_data_string.split(",")[1]
+
+    my_entity = {
+        u'PartitionKey': Coffeemachine_ID,
+        u'RowKey': Coffee_name
+    }
+
+    table_service_client = TableServiceClient.from_connection_string(
+        conn_str="<connection_string>")
+    table_client = table_service_client.get_table_client(table_name=table_name)
+
+    entity = table_client.create_entity(entity=my_entity)
